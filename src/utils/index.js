0c8ac8da32f3385a86125cb23ef31dcb675314aa
rainci <2805242676@qq.com>
2019-07-24 18:25:31 +0800

    add

--- src/utils/index.js
+++ src/utils/index.js
@@ -9,7 +9,7 @@ import { arrLevelLength } from './arrLevelLength'
 import { uniqueArray } from './uniqueArray'
 import { pickData } from './pickData'
 export {   
-    arrLevelLength,//计算数组层级数量
+    arrLevelLength,//计算数组层级 数量
     uniqueArray,//过滤数组中重复的条数
     pickData,//从对象中获取指定属性的值
 }
